5a6bfe29d941b68b6ef9e4269c5220d858580d95
Rosmary Diaz <rosmarydiaz@MacBook-Air-de-Rosmary.local>
2023-08-31 14:51:38 -0400

    cypress test

--- cypress/e2e/frontend/homePageTest.cy.js
+++ cypress/e2e/frontend/homePageTest.cy.js
@@ -1,12 +1,13 @@
 describe('testing websites', () => {
     beforeEach(() => {
-        cy.visit('https://www.cypress.io/')
+        cy.visit('https://www.atlassian.com/software/jira')
 
     })
     it ('should find Text', () => {
-        cy.title().should('include', 'Test')
-        cy.get('.nav-bar > .relative > :nth-child(3)').click()
-        //cy.get('#What-youll-learn').click()  
+        cy.title().should('include', 'Software')
+        //cy.get('.nav-bar > .relative > :nth-child(3)').click()
+        cy.xpath('//*[@id="jira"]/header/div/div[1]/div/div[2]/a[2]').click()
+        
     })
 
 })
